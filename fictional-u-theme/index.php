e66b7e7069c0b4908f5b9e659826b6a180868925
Samuel Torres <rilla@Samuels-iMac.local>
2024-03-11 23:00:11 -0400

    deleted a lot of files

---
+++ fictional-u-theme/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function createSentence($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color!</p>";
+}
+createSentence("Sam", "Gray");
+createSentence("Dan", "Purple");
+?>
+<div>
+    <h1>
+        <?php bloginfo('name'); ?>
+    </h1>
+    <h3>
+        <?php bloginfo('description'); ?>
+        iuhqwerilhuqweh
+    </h3>
+    ;qielclihqwi
+</div>
